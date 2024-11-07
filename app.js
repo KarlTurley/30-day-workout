a91751b45fb5769e84966cd0919a7470e2768d32
KarlTurley <karl@habitconsultancy.co.uk>
2024-11-07 10:42:55 +0000

    Update app.js

--- app.js
+++ app.js
@@ -125,7 +125,7 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/service-worker.js')
+  navigator.serviceWorker.register('service-worker.js')
     .then((registration) => {
       console.log('Service Worker registered with scope:', registration.scope);
     })
